449383e63774c7f147cb43b92d22decb3ddf70ab
egpivo <egpivo@gmail.com>
2026-04-03 23:31:59 +0800

    CHG: add a commit function

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use zkp::commit;
+use num_bigint::BigUint;
+
+fn main() {
+    let p = BigUint::from(23u32);
+    let g = BigUint::from(4u32);
+    let h = BigUint::from(9u32);
+
+    println!( "{}",
+        commit(&BigUint::from(333u32), &BigUint::from(3u32), &g, &h, &p)
+    )
+}
