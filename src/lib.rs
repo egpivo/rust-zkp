99434e071af8836881e9c8211e13b709131f6c83
egpivo <egpivo@gmail.com>
2026-04-05 11:55:34 +0800

    CHG: add sigma protocol

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,35 @@
-use num_bigint::BigUint;
+use num_bigint::{BigUint, RandBigInt};
 
 pub fn commit(v: &BigUint, r: &BigUint, g: &BigUint, h: &BigUint, p: &BigUint) -> BigUint {
     (g.modpow(v, p) * h.modpow(r, p)) % (p)
 }
 
+fn prove_commit(g: &BigUint, p: &BigUint) -> (BigUint, BigUint) {
+    let mut rng = rand::thread_rng();
+    let k = rng.gen_biguint_below(p);
+    let r = g.modpow(&k, p);
+    (k, r)
+}
+
+fn prove_response(k: &BigUint, e: &BigUint, secrect: &BigUint) -> BigUint {
+    k + e * secrect
+}
+
+
+pub struct Proof {
+    pub r: BigUint, // randomly commit g^k mod p 
+    pub z: BigUint, // response = k + e * secrect
+}
+
+
+
+impl Proof {
+    pub fn verify(proof: &Proof, c: &BigUint, e: &BigUint, g: &BigUint, p: &BigUint) -> bool {
+        g.modpow(&proof.z, p) == (&proof.r * c.modpow(e, p)) % p
+    }
+
+
+}
 
 #[cfg(test)]
 mod tests {
@@ -23,5 +49,24 @@ mod tests {
             commit(&BigUint::from(2u32), &BigUint::from(4u32), &g, &h, &p),
             BigUint::from(4u32)
         );
+    }
+
+    #[test]
+    fn test_sigma_protocol() {
+        let p = BigUint::from(223u32);
+        let g = BigUint::from(4u32);
+        let secrect = BigUint::from(123u32);
+        let c = g.modpow(&secrect, &p);
+
+        // Step 1: prover generates commit
+        let (k, r) = prove_commit(&g, &p);
+        // Step 2: verifier obtains challenge
+        let mut rng = rand::thread_rng();
+        let e = rng.gen_biguint_below(&p);
+        // Step 3: provers computes response
+        let z = prove_response(&k, &e, &secrect);
+        // Step 4: verification
+        let proof = Proof {r, z};
+        assert!(Proof::verify(&proof, &c, &e, &g, &p));
     }
 }
